c22f78ca7176b08ddbb941d3b47cad3a57e97a66
Anthony <ant@getme.co.uk>
2019-12-01 02:04:47 +0000

    Added exports for UI components.

--- module/index.js
+++ module/index.js
@@ -1,5 +1,31 @@
+import * as errors from './errors'
 import * as field from './field'
 import * as gallery from './gallery'
 
+import * as acceptor from './ui/acceptor'
+import * as cropTool from './ui/crop-tool'
+import * as errorMessage from './ui/error-message'
+import * as galleryItem from './ui/gallery-item'
+import * as imageEditor from './ui/image-editor'
+import * as metadata from './ui/metadata'
+import * as overlay from './ui/overlay'
+import * as uploader from './ui/uploader'
+import * as viewers from './ui/viewers'
+
+const ui = {
+    acceptor,
+    cropTool,
+    errorMessage,
+    galleryItem,
+    imageEditor,
+    metadata,
+    overlay,
+    uploader,
+    viewers
+}
+
+
+export {errors}
 export {field}
 export {gallery}
+export {ui}
